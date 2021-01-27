2d8672d8a31059290beeb85892e3775b7000fce3
Umair Zafar <umairzafariunc@gmail.com>
2021-01-27 00:29:17 -0800

    Update NewElements.js

--- src/NewElements.js
+++ src/NewElements.js
@@ -1,16 +1,76 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      display: 'flex',
+      // flexWrap: 'wrap',
+      alignItems: 'center',
+      justifyContent: 'center',
+      '& > *': {
+        margin: theme.spacing(3),
+        width: theme.spacing(50),
+        height: theme.spacing(60),
+      },
+    },
+    inputField: {
+        '& > *': {
+          
+          margin: theme.spacing(1),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '35ch',
+          height: '7ch'
+        },
+    },
+    signUpPaper: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      '& > *': {
+        margin: theme.spacing(3),
+        width: theme.spacing(50),
+        height: theme.spacing(10),
+      },
+    },  
+    
+  }));
+
+  export default useStyles
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
+    paper: {
+      display: 'flex',
+      // flexWrap: 'wrap',
+      alignItems: 'center',
+      justifyContent: 'center',
+      '& > *': {
+        margin: theme.spacing(3),
+        width: theme.spacing(50),
+        height: theme.spacing(60),
+      },
     },
-  },
-}));
+    inputField: {
+        '& > *': {
+          
+          margin: theme.spacing(1),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '35ch',
+          height: '7ch'
+        },
+    },
+    signUpPaper: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      '& > *': {
+        margin: theme.spacing(3),
+        width: theme.spacing(50),
+        height: theme.spacing(10),
+      },
+    },  
+    
+  }));
 
+  export default useStyles
